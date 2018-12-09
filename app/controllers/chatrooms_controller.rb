ea77438ab3f42fe40c554a39a974b543dff3c4a8
“Alice <aliceletourneur@icloud.com>
2018-12-09 18:48:46 +0000

    add functionnality to post messages

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,5 +3,6 @@ class ChatroomsController < ApplicationController
   def index
     @messages = Message.all.order(created_at: :asc)
     @users = User.all.order('LOWER(username)')
+    @message = Message.new
   end
 end
